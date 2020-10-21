1b6e3139edc52913c03235921103ce8de7dc94d6
Eniko Kosztolanyi <32710850+3n1k0@users.noreply.github.com>
2020-10-21 20:35:38 +0200

    refactoring cat management with vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,30 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from 'axios'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    cats: []
+  },
+  mutations: {
+    setCats: function(state, cats) {
+      state.cats = cats;
+    }
+  },
+  actions: {
+    loadCats: function({ commit }) {   
+      const headers = {
+        "x-rapidapi-key": "de7f86c3-250d-4d13-957d-fd7cac1258d9"
+      };
+      
+      axios.get("https://api.thecatapi.com/v1/breeds", {
+        headers
+      }).then((response) => {
+        commit('setCats', response.data);
+      });
+    }
+  },
   modules: {}
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
 <b-container>
-    <b-form-select v-model="selectedSortingMethod" :options="sortingOptions" v-on:change="onSortingChanged" id="dropdown-1" text="Sorting" class="m-md-2">
+    <b-form-select v-model="selectedSortingMethod" :options="sortingOptions" id="dropdown-1" text="Sorting" class="m-md-2">
     </b-form-select>
 
     <b-row align-v="center">
@@ -15,15 +15,12 @@
 import CatCard from "@/components/CatCard.vue";
 import axios from "axios";
 
-const getCats = async function () {
-    const headers = {
-        "x-rapidapi-key": "de7f86c3-250d-4d13-957d-fd7cac1258d9"
-    };
-    const response = await axios.get("https://api.thecatapi.com/v1/breeds", {
-        headers
-    });
-    return response.data.slice(0, 10);
-};
+interface Cat {
+    name: string;
+    id: string;
+    dog_friendly: number;
+    intelligence: number;
+}
 
 export default {
     name: "home",
@@ -31,10 +28,23 @@ export default {
         "cat-card": CatCard
     },
 
+    computed: {
+        cats() {
+            if (this.selectedSortingMethod === 'A-Z') {
+                return this.$store.state.cats.slice(0, 11)
+            } else {
+                const sortedCats = this.$store.state.cats.slice().sort((aCat: Cat, bCat: Cat) => {
+                    return aCat.name > bCat.name ? -1 : 1;
+                });
+
+                return sortedCats.slice(0, 11)
+            }
+        } 
+        
+    },
+
     data() {
         return {
-            cats: [],
-
             sortingOptions: [{
                     value: "A-Z",
                     text: "A-Z"
@@ -47,23 +57,8 @@ export default {
             selectedSortingMethod: "A-Z"
         }
     },
-    methods: {
-        async onSortingChanged() {
-            if (this.selectedSortingMethod === "A-Z") {
-                this.cats = await getCats(true);
-            } else {
-                const cats = await getCats(false);
-
-                const sortedCats = cats.sort((aCat: Cat, bCat: Cat) => {
-                    return aCat.name > bCat.name ? -1 : 1;
-                });
-
-                this.cats = sortedCats;
-            }
-        },
-    },
     async mounted() {
-        this.cats = await getCats();
+        this.$store.dispatch('loadCats');
     }
 };
 </script>
