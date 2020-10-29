b8696f45664565e2e2fa7d15357d0060bc9f970d
Eniko Kosztolanyi <32710850+3n1k0@users.noreply.github.com>
2020-10-30 00:13:34 +0100

    Fix console errors

--- src/components/CatCard.vue
+++ src/components/CatCard.vue
@@ -47,6 +47,11 @@ export default Vue.extend({
       
     };
     const breedIdParam = "breed_id";
+
+    if (!this.breedId) {
+      return;
+    }
+
     axios
       .get("https://api.thecatapi.com/v1/images/search", {
         headers,
@@ -56,6 +61,7 @@ export default Vue.extend({
       })
       .then((response) => {
         this.image = response.data[0].url;
+        console.log('response', response.data);
         this.temperament = response.data[0].breeds[0].temperament
           .split(",", 3)
           .join(", ");
--- src/store/index.ts
+++ src/store/index.ts
@@ -6,7 +6,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    cats: []
+    cats: [{}]
   },
   mutations: {
     setCats: function(state, cats) {
