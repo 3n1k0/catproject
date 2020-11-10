495d4ea043cc4bd84750affb35d5f196a4fdc394
Eniko Kosztolanyi <32710850+3n1k0@users.noreply.github.com>
2020-11-10 10:05:40 +0100

    use api key from env

--- src/services/thecatapi.ts
+++ src/services/thecatapi.ts
@@ -4,6 +4,7 @@ export default axios.create({
   baseURL: "https://api.thecatapi.com/v1",
   headers: {
     Accept: "application/json",
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+    "x-api-key": process.env.VUE_APP_API_KEY,
+  },
 });
